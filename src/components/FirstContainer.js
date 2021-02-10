468a8bcd07ebcc40c7efb22c1544c0dfe77cad46
DAIRYMASTER\malam <malam@dairymaster.com>
2021-02-10 12:32:30 +0000

    Second chart

--- src/components/FirstContainer.js
+++ src/components/FirstContainer.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Card , CardGroup ,Button  } from 'react-bootstrap';
-import Highcharts from 'highcharts'
-import HighchartsReact from 'highcharts-react-official'
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 
 var cardStyle = {
     backgroundColor : '#ebf0f4',
      height: '800px'
 }
+
 
 const options = {
     chart: {
@@ -140,6 +141,90 @@ const options = {
 
   }
    
+  const options2 = {
+    chart: {
+        type: 'column'
+    },
+    exporting: { enabled: false },
+    credits: {
+        enabled: false
+    },
+    title: {
+        text: 'Milk Production Overview',
+        align : 'left',
+        x: 0,
+        y: 10,
+        style : {
+            fontSize : '13px',
+            color:'#8a95a0',
+            fontWeight : '500',
+            letterSpacing : '1.5px'
+
+        }
+    },
+    // subtitle: {
+    //     text: 'Daily / Monthly'
+    // },
+    xAxis: {
+        categories: [
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+        ],
+        crosshair: true
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Total Yield monthly'
+        }
+    },
+    legend : {
+        itemStyle:{
+            color:'#8a95a0',
+            fontWeight: '500',
+            fontSize: '13px'
+        },
+
+    },
+    
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} lts</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        },
+        series: {
+            borderWidth: 0,
+            dataLabels: {
+                enabled: true,
+                format: '{point.y:.1f}%',
+                style: {fontSize: '13px' , color :'#0055a5'}
+            }
+        }
+    },
+    series: [ {
+        name: '2019',
+        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3],
+        color: '#dfdd07'
+
+    }, {
+        name: '2020',
+        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5],
+        color:'#0055a5'
+
+    }]
+  }
 
 
 class FirstContainer extends Component{
@@ -167,10 +252,12 @@ class FirstContainer extends Component{
                         Herd Overview
                     </span>
                     <hr />
-                    <HighchartsReact class="overviewpie"
-                            highcharts={Highcharts}
-                            options={options}
-                        />
+                        <div class="overviewpie">
+                            <HighchartsReact 
+                                    highcharts={Highcharts}
+                                    options={options}
+                                />
+                        </div>
   
                 </div>
             </div>
@@ -188,6 +275,87 @@ class FirstContainer extends Component{
                         Milking Stats
                     </span>
                     <hr />
+                    <div class="row ">
+                    <div class="col-6">
+                    <div class="row card_text_header">
+
+                        <div class="col-6">
+                            <a class="dashboard_link " href="#" >
+                                Cows Milked
+                            </a>
+                        </div>
+                        <div class="col-6">
+                            <a class="dashboard_link" href="#" >
+                                Total Yield (kg)
+                            </a>
+                        </div>
+                        </div>
+                        <div class="row milking_card_text">
+
+                        <div class="col-6">
+                            1256
+                        </div>
+                        <div class="col-6">
+                            17256
+                        </div>
+
+                        </div>
+                        <div class="row card_text_header" style={{paddingTop: '2%'}}>
+
+                            <div class="col-6">
+                                <a class="dashboard_link" href="#" >
+                                    Average Total Yield (kg)
+                                </a>
+                            </div>
+                            <div class="col-6">
+                                <a class="dashboard_link" href="#" >
+                                    Total Feed (kg)
+                                </a>
+                            </div>
+                            </div>
+                            <div class="row milking_card_text">
+
+                            <div class="col-6">
+                                1542
+                            </div>
+                            <div class="col-6">
+                                14523.5
+                            </div>
+
+                            </div>
+                            <div class="row card_text_header"  style={{paddingTop: '2%'}}>
+
+                            <div class="col-6">
+                                <a class="dashboard_link" href="#">
+                                    Cows Diverted
+                                </a>
+                            </div>
+                            <div class="col-6">
+                                <a class="dashboard_link" href="#">
+                                    Milking Duration
+                                </a>
+                            </div>
+                            </div>
+                            <div class="row milking_card_text">
+
+                            <div class="col-6">
+                                9%
+                            </div>
+                            <div class="col-6">
+                                05:51
+                            </div>
+
+                            </div>
+                        </div>
+                        <div class="col-6">
+                        <div class="overviewpie">
+                        <HighchartsReact 
+                            highcharts={Highcharts}
+                            options={options2}
+                        />
+                        </div>
+                        </div>
+                    </div>
                    
                 </div>
             </div>
